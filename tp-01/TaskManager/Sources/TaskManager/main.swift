019dd7bb7d3fabd63ad3fcf011b6f7bcba495db5
AlexandraBoudry <alexandra.boudry@etu.unige.ch>
2017-10-12 16:56:36 +0200

    TP01+comments

--- tp-01/TaskManager/Sources/TaskManager/main.swift
+++ tp-01/TaskManager/Sources/TaskManager/main.swift
@@ -10,7 +10,7 @@ let taskManager = createTaskManager()
 let create = taskManager.transitions.filter{$0.name=="create"}[0]
 let spawn = taskManager.transitions.filter{$0.name=="spawn"}[0]
 let exec = taskManager.transitions.filter{$0.name=="exec"}[0]
-let sucess = taskManager.transitions.filter{$0.name=="success"}[0]
+let success = taskManager.transitions.filter{$0.name=="success"}[0]
 let fail = taskManager.transitions.filter{$0.name=="fail"}[0]
 
 let taskPool = taskManager.places.filter{$0.name=="taskPool"}[0]
@@ -19,18 +19,23 @@ let inProgress = taskManager.places.filter{$0.name=="inProgress"}[0]
 
 
 let m1 = create.fire(from:[taskPool: 0, processPool: 0, inProgress: 0])
-print(m1)
+print(m1!)
 let m2 = spawn.fire(from: m1!)
-print(m2)
+print(m2!)
 let m3 = spawn.fire(from: m2!)
-print(m3)
+print(m3!)
 let m4 = exec.fire(from: m3!)
-print(m4)
-let m5 = sucess.fire(from: m4!)
-print(m5)
-let m6 = exec.fire(from: m5!)
-print(m6)
+print(m4!)
+let m5 = exec.fire(from: m4!)
+print(m5!)
+let m6 = success.fire(from: m5!)
+print(m6!)
+let m7 = fail.fire(from: m6!)
+print(m7!)
 
+// Dans le cas original, cela pose problème lorsque l'on a une tâche et deux processus car le deuxième processus
+//peut executer la tâche alors qu'elle a déjà été executée avec un processus.Toutefois, le deuxième processus resta
+//bloqué dans inProgress et la seule façon de le suprimer sera de tirer fail.
 
 
 let correctTaskManager = createCorrectTaskManager()
@@ -43,7 +48,7 @@ let correctTaskManager = createCorrectTaskManager()
 let create2 = correctTaskManager.transitions.filter{$0.name=="create"}[0]
 let spawn2 = correctTaskManager.transitions.filter{$0.name=="spawn"}[0]
 let exec2 = correctTaskManager.transitions.filter{$0.name=="exec"}[0]
-let sucess2 = correctTaskManager.transitions.filter{$0.name=="success"}[0]
+let success2 = correctTaskManager.transitions.filter{$0.name=="success"}[0]
 let fail2 = correctTaskManager.transitions.filter{$0.name=="fail"}[0]
 
 let taskPool2 = correctTaskManager.places.filter{$0.name=="taskPool"}[0]
@@ -53,14 +58,19 @@ let newPlace2 = correctTaskManager.places.filter{$0.name=="newPlace"}[0]
 
 
 let m21 = create2.fire(from:[taskPool2: 0, processPool2: 0, inProgress2: 0, newPlace2: 0])
-print(m21)
+print(m21!)
 let m22 = spawn2.fire(from: m21!)
-print(m22)
+print(m22!)
 let m23 = spawn2.fire(from: m22!)
-print(m23)
+print(m23!)
 let m24 = exec2.fire(from: m23!)
-print(m24)
-let m25 = sucess2.fire(from: m24!)
-print(m25)
-let m26 = exec2.fire(from: m25!)
-print(m26)
+print(m24!)
+let m25 = success2.fire(from: m24!)
+print(m25!)
+
+// En ajoutant une place en précondition de exec, la suite où on tire deux fois de suite exec n'est plus possible.
+//En effet, exec ne pourra être tiré que si une nouvelle tâche est créée.
+//Le processus attendra d'avoir une tâche pour pouvoir s'exécuter. Une fois la tâche exécutée avec le processus,
+//soit on tire success, soit on tire fail. Dans le cas où sucess est tiré, la tâche et le prcessus sont détruits.
+//Dans le cas où fail est tiré, le processus est détruit et un token est placé dans newPlace afin d'attendre un nouveau
+//processus pour pouvoir tirer exéc à nouveau afin de traiter la tâche.
--- tp-01/TaskManager/Sources/TaskManagerLib/TaskManagerLib.swift
+++ tp-01/TaskManager/Sources/TaskManagerLib/TaskManagerLib.swift
@@ -41,29 +41,30 @@ public func createCorrectTaskManager() -> PTNet {
     let taskPool    = PTPlace(named: "taskPool")
     let processPool = PTPlace(named: "processPool")
     let inProgress  = PTPlace(named: "inProgress")
-    let newPlace  = PTPlace(named: "newPlace") //Je crée une nouvelle place, qui a comme précondition exec et comme postconditions sucess et fail
+    let newPlace  = PTPlace(named: "newPlace") //Je crée une nouvelle place, qui est une précondition de exec
+    // et une postcondition de create et fail
 
     // Transitions
     let create      = PTTransition(
         named          : "create",
         preconditions  : [],
-        postconditions : [PTArc(place: taskPool)])
+        postconditions : [PTArc(place: taskPool), PTArc(place: newPlace)]) // newPlace est une postcondition de create
     let spawn       = PTTransition(
         named          : "spawn",
         preconditions  : [],
         postconditions : [PTArc(place: processPool)])
     let success     = PTTransition(
         named          : "success",
-        preconditions  : [PTArc(place: taskPool), PTArc(place: inProgress), PTArc(place: newPlace)], // newPlace est une précondition de sucess
+        preconditions  : [PTArc(place: taskPool), PTArc(place: inProgress)],
         postconditions : [])
     let exec       = PTTransition(
         named          : "exec",
-        preconditions  : [PTArc(place: taskPool), PTArc(place: processPool)],
-        postconditions : [PTArc(place: taskPool), PTArc(place: inProgress), PTArc(place: newPlace)]) // newPlace est une postcondition de exec
+        preconditions  : [PTArc(place: taskPool), PTArc(place: processPool), PTArc(place: newPlace)], // newPlace est une précondition de exec
+        postconditions : [PTArc(place: taskPool), PTArc(place: inProgress)])
     let fail        = PTTransition(
         named          : "fail",
-        preconditions  : [PTArc(place: inProgress), PTArc(place: newPlace)], // newPlace est une précondition de fail
-        postconditions : [])
+        preconditions  : [PTArc(place: inProgress)],
+        postconditions : [PTArc(place: newPlace)])  // newPlace est une postcondition de fail
 
     // P/T-net
     return PTNet(
